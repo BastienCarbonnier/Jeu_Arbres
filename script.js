a3c52ab157d6b054444d1af466e4b89cd0c7ae3c
Bastien Carbonnier <bastien.carbonnier@gmail.com>
2017-10-25 09:27:53 +0200

    Rajout fichier

---
+++ script.js
@@ -0,0 +1,34 @@
+var expression;
+var op =['→','¬','∧','∨']
+
+
+function init(){
+	var expression ="¬(p→((p→q)→q))";
+	interface = document.getElementById("interface");
+
+	var expGraph = document.createElement('p');
+	expGraph.setAttribute('class','exp');
+	expGraph.innerHTML = expression;
+
+
+}
+
+function parentheseFermante(expr, index){
+	var i = index+1;
+	var trouve = false;
+	var para = 1;
+	while (i<expr.length && para!=0){
+		if(index[i]=='('){
+			para++;
+		}
+		if(index[i]==')'){
+			para--;
+		}
+		i++;
+	}
+	return i;
+}
+
+function decompEnDeux(){
+
+}
