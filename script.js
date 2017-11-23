279dba5b9843a9f56c28023e48a25945b18b0d0e
Bastien Carbonnier <bastien.carbonnier@gmail.com>
2017-11-23 22:23:07 +0100

    Rajout decoupe expression en 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var expression;
-var op =['→','¬','∧','∨']
+var op =['→','¬','∧','∨'];
 
 
 function init(){
@@ -11,57 +11,114 @@ function init(){
 	expGraph.innerHTML = expression;
 
 	interface.appendChild(expGraph);
-    console.log(trouverParentheseFermante("((Salu))t",0));
-    console.log(trouverParentheseFermante("((Salu)thziz)",1));
+	exp = parserExpressionEnDeux("(a∧b)∧¬(bvc)");
+	afficherExpression(exp);
+	exp = parserExpressionEnDeux("¬a");
+	afficherExpression(exp);
+	exp = parserExpressionEnDeux("¬a∧b");
+	afficherExpression(exp);
+	exp = parserExpressionEnDeux("¬av(c∧b)");
+	afficherExpression(exp);
 }
 
-var Expression = function (){};
-
+function Element (val,neg,taille){
+	this.val = val;
+	this.neg = neg;
+	this.taille = taille;
+}
 function Expression (elt1,symbole,elt2){
-    
-}
-function trouverElement(exp){
-    var elt = new String("");
-    var neg =false;
-    
-    var ind_par_f = 0;
-    var ind_par_o = 0;
-    
-    
-    if (exp[0] === "¬"){
-        neg = true;
-        
-        ind_par_f = trouverParentheseFermante(exp,1);
-        ind_par_o = 1;
-        
-    }
-    else if (exp[0] === "("){
-        ind_par_f = trouverParentheseFermante(exp,0);
-        ind_par_o = 0;
-    }
-    
-    if (ind_par_f==0)
-        elt = exp;
-    else
-        elt = exp.substr(ind_par_o,ind_par_f);
-    
-}
-function decoupage(){
-    
+	this.elt1 = elt1;
+	this.symbole = symbole;
+	this.elt2 = elt2;
 }
 
-function trouverParentheseFermante(expression, indice){
+function parserExpression (exp){
+
+}
+
+function resoudreEquation (exp){
+	var e = parserExpressionEnDeux(exp);
+	var elt1,elt2;
+	switch(e.symbole) {
+	    case "→":
+	        e.elt1.neg = !e.elt1.neg;
+
+	        break;
+	    case "∧":
+
+	        break;
+		case "∨":
+
+			break;
+	    default:
+			console.log("");
+}
+}
+
+function parserExpressionEnDeux(exp){
+	var symbole;
+
+	elt1 = trouverElement(exp);
+	symbole = exp[elt1.taille];
+	elt2 = trouverElement(exp.substr(elt1.taille+1));
+
+	return new Expression (elt1, symbole, elt2);
+
+}
+function afficherExpression(exp){
+	console.log(exp.elt1.val + "     " +exp.symbole + "      "+ exp.elt2.val);
+
+	console.log(exp.elt1.neg + "     " +exp.symbole + "      "+ exp.elt2.neg);
+}
+
+
+function trouverElement(exp){
+	var elt = new String("");
+	var neg =false;
+	var taille = 0;
+	var ind_par_f = 0;
+	var ind_par_o = 0;
+
+
+	if (exp[0] === "¬"&& exp[1] === "("){
+		neg = true;
+		ind_par_f = trouverParentheseFermante(exp,1)-1;
+		ind_par_o = 1;
+		taille += 1;
+	}
+	else if (exp[0] === "("){
+		ind_par_f = trouverParentheseFermante(exp,0);
+		ind_par_o = 0;
+	}
+	else if (exp[0] === "¬"){
+		neg = true;
+		elt = exp.substr(1,1);
+		taille = 2;
+		return new Element(elt,neg,taille);
+	}
+
+	if (ind_par_f==0){
+		elt = exp;
+		taille = exp.length;
+	}
+	else{
+		elt = exp.substr(ind_par_o+1,ind_par_f-1);
+		taille = taille + elt.length + 2;
+	}
+	return new Element(elt,neg,taille);
+}
+
+function trouverParentheseFermante(exp, indice){
 	var trouve = false;
 	var par = 0;
-    expression = expression.substr(index);
-	for (i in expression){
-        if (expression[i]==="(")
-            par++;
-        else if (expression[i]===")")
-            par--;
-        if (par == 0)
-            break;
+	exp = exp.substr(indice);
+	for (i in exp){
+		if (exp[i]==="(")
+			par++;
+		else if (exp[i]===")")
+			par--;
+		if (par == 0)
+			break;
 	}
 	return (new Number(i)+new Number(indice));
 }
-
