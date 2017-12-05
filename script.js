12bd1fe7e17594fa12dff390b466d9f630765a4f
Bastien Carbonnier <bastien.carbonnier@gmail.com>
2017-12-05 21:38:25 +0100

    Rajout calculs score et memorisation score précédent

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var op =['→','¬','∧','∨'];
+var formules = [
+	"p→((p→q)→q)",
+	"p→(¬q→¬(p→q))",
+	"(¬p→¬q)→(q→p)",
+	"(p∧(p→q)∧((p→q)→r))→(p∧q∧r)",
+	"(p→q)→((q→r)→(p→r))",
+	"(((s∧p)→(q∧r))∧(¬r∨¬q)∧p)→¬s",
+	"((p→q)∧((r∧s)→p)∧(t→r)∧(s∧t))→q",
+	"(p→(q→r))→((p→q)→(p→r))",
+	"(q→p)→((¬q→p)→p)",
+	"p→(q→r)→((p→r)∨(q→r))",
+	"((p→r)∨(q→r))→(p→(q→r))",
+	"((p→q)∧(q→p))∨(p∧¬q)∨(¬p∧q)",
+	"(¬(a∧b)→(¬a∨¬b))",
+	"((¬a∨¬b)→¬(a∧b))",
+	"((a∨b∨¬c)∧(a∨b∨c)∧(a∨¬b))→a",
+	"(u∧(w→v)∧(t→v)∧(u→(w∨t)))→v",
+	"r∨((p∧(p→q)∧((p→q)→r))→(p∧q∧r))∧(t→¬r)",
+	"(p∨(q→¬p))∨((p∧(p→q)∧((p→q)→r))→(p∧q∧r))",
+	"((p→(q→r))→((q→r)∨(q→r)))→((¬(¬q→¬p))∨¬q∨q)",
+	"((((s∧p)→(q∧r))∧(¬r∨¬q)∧p)∧(t∧(s→¬t)))→¬s"
+];
+var scores = new Array();
+
 
 function init(){
 	var expression ="¬¬(p→((p→q)→q))";
@@ -16,6 +39,16 @@ function init(){
 	//testResoudreEquation("av(c∧b)");
 	//testResoudreEquation("¬(a→(c∧b))");
 	//testParserExpression("¬(a→(¬c∧b))");
+	/*var expr = "¬(((A→C)∨(B→C))→((A∨B)→C))";
+	var expr2 = "(p∨(q→¬p))∨((p∧(p→q)∧((p→q)→r))→(p∧q∧r))";
+*/
+	var expr = obtenirFormuleAleatoire();
+
+}
+function reinitialiserScores(){
+	for (i in formules){
+		localStorage.setItem(formules[i],Infinity);
+	}
 }
 
 function Element (val,neg,taille){
@@ -56,8 +89,8 @@ function recupererStringElement (elt){
 function parserExpression (exp){
 
 	var e = resoudreEquation(exp); // Renvoi une Expression
-	console.log("Dans parser Expression : e = ");
-	afficherExpression(e);
+	//console.log("Dans parser Expression : e = ");
+	//afficherExpression(e);
 	var elt1_string, elt2_string="";
 
 	if (e.neg){
@@ -72,20 +105,32 @@ function parserExpression (exp){
 
 	return new ExpressionAffichage(elt1_string,symbole,elt2_string);
 }
+
+function recupererStringExpressionAffichage (exp_aff){
+	var res = "";
+	if (exp_aff.symbole === "¬"){
+		res = "¬(" + exp_aff.elt1 + ")";
+	}
+	else{
+		res = exp_aff.elt1 + exp_aff.symbole + exp_aff.elt2;
+	}
+	return res;
+}
 function inverserElement (elt){
 	return new Element(elt.val, !elt.neg, elt.taille);
 }
 function resoudreEquation(exp){
 	var e = parserExpressionEnDeux(exp);
-	console.log("expression : "+exp);
-	console.log("Premier parsage : ");
-	afficherExpression(e);
+	//console.log("expression : "+exp);
+	//console.log("Premier parsage : ");
+	//afficherExpression(e);
+
 	var symbole;
 	var neg =false;
 	if (e.neg){
 		e2 = parserExpressionEnDeux(e.elt1.val);
-		console.log("Deuxième parsage : ");
-		afficherExpression(e2);
+		//console.log("Deuxième parsage : ");
+		//afficherExpression(e2);
 		switch(e2.symbole) {
 			case "→":
 				elt1 = e2.elt1;
@@ -104,7 +149,7 @@ function resoudreEquation(exp){
 			break;
 			case "¬":
 				elt1 = inverserElement(e2.elt1);
-				symbole = "";
+				symbole = "¬";
 				neg = true;
 			break;
 			default:
@@ -131,8 +176,11 @@ function resoudreEquation(exp){
 function parserExpressionEnDeux(exp_string){
 	var symbole;
 	var neg =false;
-
 	elt1 = trouverElement(exp_string);
+
+	if (elt1 === "undefined"){
+		return "undefined";
+	}
 
 	if ((elt1.taille === exp_string.length) && elt1.neg){
 		symbole = "¬";
@@ -177,6 +225,10 @@ function trouverElement(exp){
 		elt = exp.substr(1);
 		taille = exp.length;
 	}
+	else if (exp === "undefined"){
+		elt = "";
+		taille = 0;
+	}
 	else{
 		elt = exp[0];
 		taille = 1;
@@ -205,7 +257,54 @@ function afficherExpression(exp){
 
 	console.log(exp.elt1.neg + "     " +exp.symbole + "      "+ exp.elt2.neg);
 }
+function verifMeilleurScore(nb_mis, exp_string){
+	//var max = calculerNbEtapesMax(exp_string);
+	var nb_save = localStorage.getItem(exp_string);
+	if (nb_mis<nb_save){
+		nb_save = nb_mis;
+		return true;
+	}
+	return false;
 
+}
+
+function calculerNbEtapesMax(exp_string){
+	if (exp_string.length <=2)
+		return 0;
+	var exp = resoudreEquation(exp_string);
+	console.log ("Debut calculerNbEtapes()    ");
+	if (exp === "undefined"){
+		return 0;
+	}
+	afficherExpression(exp);
+
+	var op = ["∨","∧","→","¬"];
+	if (!op.includes(exp.symbole)){
+		return 0;
+	}
+	else if (exp.symbole === "¬"){
+		console.log(recupererStringElement(exp.elt1));
+		return 1
+			+ calculerNbEtapesMax(recupererStringElement(exp.elt1));
+	}
+	else {
+		return 2
+			+ calculerNbEtapesMax(recupererStringElement(exp.elt1))
+			+ calculerNbEtapesMax(recupererStringElement(exp.elt2));
+	}
+}
+
+function obtenirFormuleAleatoire (){
+	return formules[Math.floor(Math.random()*formules.length)];
+}
+function testCalculerNbEtapes(exp){
+	var nb = calculerNbEtapesMax(exp);
+	console.log("nb d'étapes = "+nb);
+}
+function testCalculerScore(nb_mis,exp){
+	var res = calculerScore(nb_mis,exp);
+	console.log("resultat   =   " + res);
+}
 function testParserExpression(expression){
 	exp = parserExpression(expression);
 	afficherExpressionAffichage(exp);
